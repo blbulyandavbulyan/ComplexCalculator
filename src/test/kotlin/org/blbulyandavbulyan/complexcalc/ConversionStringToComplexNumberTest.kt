d22e2eb140b43bb7449546a0927e7cf33cf2bc24
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-21 20:55:50 +0400

    ComplexNumberParser is already removed, so this class is renamed and fixing compile error

--- src/test/kotlin/org/blbulyandavbulyan/complexcalc/ConversionStringToComplexNumberTest.kt
+++ src/test/kotlin/org/blbulyandavbulyan/complexcalc/ConversionStringToComplexNumberTest.kt
@@ -2,41 +2,37 @@ package org.blbulyandavbulyan.complexcalc
 
 import org.blbulyandavbulyan.complexcalc.complex.ComplexNumber
 import org.blbulyandavbulyan.complexcalc.complex.exceptions.WrongComplexNumber
+import org.blbulyandavbulyan.complexcalc.complex.toComplex
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class ComplexNumberParserTest {
-    private lateinit var parser: ComplexNumberParser
-
+class ConversionStringToComplexNumberTest {
     @BeforeEach
-    fun setUp() {
-        parser = ComplexNumberParser()
-    }
     @Test
     fun `test parse with correct data`(){
-        assertEquals(ComplexNumber(2.0,2.0), parser.parse("2 + 2i"))
-        assertEquals(ComplexNumber(2.323, 2.23), parser.parse("2.323 + 2.23i"))
-        assertEquals(ComplexNumber(-2.2, -2.2), parser.parse("-2.2 - 2.2i"))
-        assertEquals(ComplexNumber(2.2, -2.2324), parser.parse("2.2 - 2.2324i"))
+        assertEquals(ComplexNumber(2.0,2.0),"2 + 2i".toComplex())
+        assertEquals(ComplexNumber(2.323, 2.23), "2.323 + 2.23i".toComplex())
+        assertEquals(ComplexNumber(-2.2, -2.2), "-2.2 - 2.2i".toComplex())
+        assertEquals(ComplexNumber(2.2, -2.2324), "2.2 - 2.2324i".toComplex())
     }
     @Test
     fun `should throw if no i letter added`(){
         assertThrows(WrongComplexNumber::class.java) {
-            parser.parse("2 + 2");
+            "2 + 2".toComplex()
         }
     }
     @Test
     fun `should throw if i letter in wrong place`(){
         assertThrows(WrongComplexNumber::class.java){
-            parser.parse("2i + 2")
+            "2i + 2".toComplex()
         }
         assertThrows(WrongComplexNumber::class.java){
-            parser.parse("2i.2 + 2")
+            "2i.2 + 2".toComplex()
         }
         assertThrows(WrongComplexNumber::class.java){
-            parser.parse("2i + 2i")
+            "2i + 2i".toComplex()
         }
     }
 }
