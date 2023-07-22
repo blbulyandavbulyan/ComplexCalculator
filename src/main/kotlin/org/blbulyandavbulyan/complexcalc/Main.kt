7041db695045dd5f7d9a08d0907627edef09e5c6
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-22 12:25:44 +0400

    ExpressionParser moved to expressions package, now this class doesn't calculate anything, it just parses expression

--- src/main/kotlin/org/blbulyandavbulyan/complexcalc/Main.kt
+++ src/main/kotlin/org/blbulyandavbulyan/complexcalc/Main.kt
@@ -2,25 +2,27 @@ package org.blbulyandavbulyan.complexcalc
 
 import org.blbulyandavbulyan.complexcalc.calc.Calculator
 import org.blbulyandavbulyan.complexcalc.calc.CalculatorWithLogging
-import org.blbulyandavbulyan.complexcalc.calc.ExpressionParser
+import org.blbulyandavbulyan.complexcalc.expressions.ExpressionParser
 import org.blbulyandavbulyan.complexcalc.calc.exceptions.WrongExpressionException
+import org.blbulyandavbulyan.complexcalc.complex.ComplexNumber
 import org.blbulyandavbulyan.complexcalc.complex.exceptions.WrongComplexNumber
+import org.blbulyandavbulyan.complexcalc.expressions.Expression
 import java.util.Scanner
 import java.util.logging.FileHandler
-
+private fun Calculator.calculate(expression: Expression): ComplexNumber = this.calculate(expression.operand1, expression.operand2, expression.operation)
 fun main() {
     val sc = Scanner(System.`in`)
     val calculator = CalculatorWithLogging()
     val fileHandler = FileHandler("log.txt")
     calculator.logger.addHandler(fileHandler)
     calculator.logger.useParentHandlers = false
-    val expressionParser = ExpressionParser(calculator)
+    val expressionParser = ExpressionParser()
     while (true){
         try {
-            val expression = sc.nextLine()
-            if(expression == "EXIT" || expression == "exit") return
-            val result = expressionParser.parse(expression)
-            println(result)
+            val expressionString = sc.nextLine()
+            if(expressionString == "EXIT" || expressionString == "exit") return
+            val expression = expressionParser.parse(expressionString)
+            println(calculator.calculate(expression))
         }
         catch (e: RuntimeException){
             if(e is WrongComplexNumber || e is WrongExpressionException) println(e.message)
--- src/main/kotlin/org/blbulyandavbulyan/complexcalc/expressions/ExpressionParser.kt
+++ src/main/kotlin/org/blbulyandavbulyan/complexcalc/expressions/ExpressionParser.kt
@@ -1,21 +1,20 @@
-package org.blbulyandavbulyan.complexcalc.calc
+package org.blbulyandavbulyan.complexcalc.expressions
 
+import org.blbulyandavbulyan.complexcalc.calc.Operation
 import org.blbulyandavbulyan.complexcalc.calc.exceptions.WrongExpressionException
-import org.blbulyandavbulyan.complexcalc.complex.ComplexNumber
 import org.blbulyandavbulyan.complexcalc.complex.exceptions.WrongComplexNumber
 import org.blbulyandavbulyan.complexcalc.complex.toComplex
 import java.lang.IllegalArgumentException
 
 /**
- * Данный класс предназначен для разбора выражений, которые необходимо вычислить с участием комплексных чисел
- * @param calculator с помощью которого будут производиться вычисления над комплексными числами
+ * Данный класс предназначен для разбора преобразования строки в выражение
  */
-class ExpressionParser(private val calculator: Calculator) {
+class ExpressionParser {
     /**
      * Данная функция вычисляет выражение в строковом формате с участием комплексных чисел
      * @param expression выражение, которое нужно вычислить
      */
-    fun parse(expression: String): ComplexNumber {
+    fun parse(expression: String): Expression {
         //здесь, первая группа в регулярном выражении - первый операнд
         //вторая группа оператор(оно же прописано в enum Operation)
         //третья группа - второй операнд
@@ -27,7 +26,7 @@ class ExpressionParser(private val calculator: Calculator) {
             val a = groupValues[1].toComplex()
             val b = groupValues[3].toComplex()
             val operation = Operation.valueOf(groupValues[2])
-            return calculator.calculate(a, b, operation)
+            return Expression(a, b, operation)
         }
         catch (e: IllegalArgumentException){
             throw WrongExpressionException("You gave a wrong expression, operation is invalid!", e)
