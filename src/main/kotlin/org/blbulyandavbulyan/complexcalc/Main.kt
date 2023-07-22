97b8482fce298b4c351f9105021cc4ed7f7016b2
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-22 11:23:14 +0400

    Method parseExpression removed from Calculator, and Main now uses class ExpressionParser for parsing expressions instead Calculator

--- src/main/kotlin/org/blbulyandavbulyan/complexcalc/Main.kt
+++ src/main/kotlin/org/blbulyandavbulyan/complexcalc/Main.kt
@@ -1,16 +1,18 @@
 package org.blbulyandavbulyan.complexcalc
 
 import org.blbulyandavbulyan.complexcalc.calc.Calculator
+import org.blbulyandavbulyan.complexcalc.calc.ExpressionParser
 import java.util.Scanner
 
 fun main() {
     val sc = Scanner(System.`in`)
     val calculator = Calculator()
+    val expressionParser = ExpressionParser(calculator)
     while (true){
         try {
             val expression = sc.nextLine()
             if(expression == "EXIT" || expression == "exit") return
-            val result = calculator.processExpression(expression)
+            val result = expressionParser.parse(expression)
             println(result)
         }
         catch (e: RuntimeException){
